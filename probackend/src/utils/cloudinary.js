c21fbaa1cba27a3ced79b67f32e2e5bbcea10982
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-05-11 18:18:30 +0600

    upload to cloudinary and multer basic discussion

---
+++ probackend/src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SERECT,
+});
+
+
+const uploadOnCloudinary = async (localFilePath)=> {
+  try {
+    if(!localFilePath) return 'could not find local file';
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: 'auto',
+    });
+    //file has been uploaded successfully
+    console.log('file is uploaded successfully on cloudinary', response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //removes the locally uploaded temporary file as the upload operation failed
+    return null; 
+  }
+};
+
+
+export {uploadOnCloudinary};
