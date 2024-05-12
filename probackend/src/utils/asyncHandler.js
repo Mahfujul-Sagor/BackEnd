45cc8bd2684a9fab436b2aa4778b9c8e6f5bd90e
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-05-12 17:57:02 +0600

    all setup configures

--- probackend/src/utils/asyncHandler.js
+++ probackend/src/utils/asyncHandler.js
@@ -1,12 +1,21 @@
-const asyncHandler = (fn)=> async (req, res, next)=> {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message
-    });
-  }
+//with promise
+const asyncHandler = (requestHandler)=> {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err)=>next(err));
+  };
 };
+
+
+// with try catch
+// const asyncHandler = (fn)=> async (req, res, next)=> {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message
+//     });
+//   }
+// };
 
 export {asyncHandler};
